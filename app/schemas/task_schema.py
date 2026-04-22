450d667afee1d1beebe5d832da6997df720dae2f
Jareel Ireri <jareel.ireri@student.moringaschool.com>
2026-04-22 20:18:04 +0300

    Feature: Add Marshmallow schemas

---
+++ app/schemas/task_schema.py
@@ -0,0 +1,11 @@
+from app import ma
+from app.models.task import Task
+
+class TaskSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Task
+        load_instance = True
+        include_fk = True  # Includes the user_id in the JSON
+
+task_schema = TaskSchema()
+tasks_schema = TaskSchema(many=True)
